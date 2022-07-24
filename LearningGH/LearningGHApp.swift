cf5177712bf6cd6fa7c2fa20aae50446050814d9
Imaa <wmohammad83@icloud.com>
2022-07-24 13:55:42 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Waseem Mohammad on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
